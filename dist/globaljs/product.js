c7a58180c53ac9c29424c36baa33aab1396f5155
duwscan <109639466+DuwscAn@users.noreply.github.com>
2022-12-04 21:33:44 +0700

    Create product.js

---
+++ dist/globaljs/product.js
@@ -0,0 +1,198 @@
+class Glass {
+  constructor(
+    brand,
+    name,
+    description,
+    size = [],
+    price,
+    isSale,
+    salePrice,
+    amount,
+    feature,
+    id
+  ) {
+    this.id = id;
+    this.brand = brand;
+    this.name = name;
+    this.description = description;
+    this.size = size;
+    this.price = price;
+    this.isSale = isSale;
+    this.salePrice = salePrice;
+    this.amount = amount;
+    this.feature = feature;
+  }
+}
+const listProducts = [
+  new Glass(
+    'Fendi',
+    'Lorem Glass',
+    'Dummy Info',
+    ['XL', 'Xs'],
+    300,
+    false,
+    0,
+    4,
+    'rcmd',
+    1
+  ),
+  new Glass(
+    'Gucci',
+    'Lorem 2 Glass',
+    'Dummy Info',
+    ['XL', 'Xs'],
+    300,
+    false,
+    0,
+    4,
+    'rcmd',
+    2
+  ),
+  new Glass(
+    'Fendi',
+    'Lorem Glass',
+    'Dummy Info',
+    ['Xs'],
+    300,
+    false,
+    0,
+    4,
+    'rcmd',
+    3
+  ),
+  new Glass(
+    'Versace',
+    'Lorem Glass',
+    'Dummy Info dhasdasjgdas',
+    ['L', 'MD', 'SM'],
+    300,
+    true,
+    250,
+    3,
+    'rcmd',
+    4
+  ),
+];
+// feature : rcmd new men women 
+class ListGlass {
+  constructor(listOfGlass) {
+    const storaged = window.localStorage.getItem('listProducts')
+      ? JSON.parse(window.localStorage.getItem('listProducts'))
+      : [];
+    this.list = [...storaged, ...listOfGlass].filter(
+      (v, i, a) => a.findIndex((v2) => v2.id === v.id) === i
+    );
+    window.localStorage.setItem('listProducts', JSON.stringify(this.list));
+  }
+  static getListProduct() {
+    return window.localStorage.getItem('listProducts')
+      ? JSON.parse(window.localStorage.getItem('listProducts'))
+      : [];
+  }
+  groupAvailableBrand() {
+    const glassesBrands = this.list.map((glass) => glass.brand);
+    const availableBrand = glassesBrands.filter((brand, index) => {
+      return glassesBrands.indexOf(brand) === index;
+    });
+    return availableBrand;
+  }
+  groupByFeature() {
+    return list.filter((glass) => glass.feature === feature);
+  }
+
+  // filter section
+  static groupBySize(list = [], size) {
+    return list.filter((glass) => {
+      return glass.size.includes(size);
+    });
+  }
+  static groupByBrand(list = [], brand) {
+    return list.filter((glass) => glass.brand === brand);
+  }
+
+  sortDescPrice(list = []) {
+    return list.sort(
+      (glassA, glassB) =>
+        (glassA.isSale ? glassA.salePrice : glassA.price) -
+        (glassB.isSale ? glassB.salePrice : glassB.price)
+    );
+  }
+
+  sortDescPrice(list = []) {
+    return list.sort(
+      (glassA, glassB) =>
+        (glassA.isSale ? glassA.salePrice : glassA.price) -
+        (glassB.isSale ? glassB.salePrice : glassB.price)
+    );
+  }
+
+  sortAscPrice(list = []) {
+    return list.sort(
+      (glassA, glassB) =>
+        (glassB.isSale ? glassB.salePrice : glassB.price) -
+        (glassA.isSale ? glassA.salePrice : glassA.price)
+    );
+  }
+}
+
+const listGlass = new ListGlass(listProducts);
+class PayGlass extends Glass {
+  constructor(
+    brand,
+    name,
+    description,
+    size,
+    price,
+    isSale,
+    salePrice,
+    amount,
+    feature,
+    id,
+    buyAmount
+  ) {
+    super(
+      brand,
+      name,
+      description,
+      size,
+      price,
+      isSale,
+      salePrice,
+      amount,
+      feature,
+      id
+    );
+    //TODO
+  }
+}
+class Cart {
+  constructor(listOfGlassBuy) {
+    const storaged = window.localStorage.getItem('cart')
+      ? JSON.parse(window.localStorage.getItem('cart'))
+      : [];
+    this.list = [...storaged, ...listOfGlass].filter(
+      (v, i, a) => a.findIndex((v2) => v2.id === v.id) === i
+    );
+    this.saveLocal();
+  }
+  saveLocal() {
+    window.localStorage.setItem('cart', JSON.stringify(this.list));
+  }
+  addProduct(product) {
+    this.list.push(product);
+    saveLocal();
+  }
+
+  deleteProduct(productId) {
+    this.list = this.list.filter((product) => product.id !== productId);
+    saveLocal();
+  }
+  afterPayment() {
+    window.localStorage.removeItem('cart');
+  }
+  static getCartProduct() {
+    return window.localStorage.getItem('listProducts')
+      ? JSON.parse(window.localStorage.getItem('listProducts'))
+      : [];
+  }
+}
