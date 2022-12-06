4bd16aded0e1e4b5309ff8da30b8d6cb54bf5f8a
duwscan <109639466+DuwscAn@users.noreply.github.com>
2022-12-06 12:58:18 +0700

    update link cart icon to cart pages

--- dist/layout/header/asset/js/nav.js
+++ dist/layout/header/asset/js/nav.js
@@ -1,30 +1,28 @@
-
 const navBar = document.querySelector('nav');
 const menu = document.querySelector('.nav-bar__toggleButton');
 const menuIcon = document.querySelector('.nav-bar__logo');
-const navItems = document.querySelectorAll(".nav-bar__items-item");
+const navItems = document.querySelectorAll('.nav-bar__items-item');
 let startTime = 400;
 let pos = 0;
-navItems.forEach(element=>{
-  let myStyle =
-  `
+navItems.forEach((element) => {
+  let myStyle = `
     top:${pos}%;
     transition-duration:${startTime}ms;
-  `
-  element.style.cssText=myStyle;
-  pos+=100;
-  startTime+=200;
-})
-let isOpen= false;
+  `;
+  element.style.cssText = myStyle;
+  pos += 100;
+  startTime += 200;
+});
+let isOpen = false;
 menu.addEventListener('click', () => {
-  navItems.forEach(element => {
-   element.classList.toggle('nav-bar--open');
+  navItems.forEach((element) => {
+    element.classList.toggle('nav-bar--open');
   });
-  const check = document.querySelector(".nav-bar__items-item");
-if(check.classList.contains('nav-bar--open')){
+  const check = document.querySelector('.nav-bar__items-item');
+  if (check.classList.contains('nav-bar--open')) {
     menu.innerHTML = `<i class="nav-bar__logo ti ti-x"></i>`;
-}else{
+  } else {
     menu.innerHTML = `<i class="nav-bar__logo ti ti-menu-2"></i>`;
-}
+  }
 });
 
