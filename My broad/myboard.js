2f273a8d91b8f51abae1039f8673ce53e8eb2827
namnguyen2702 <112916393+namnguyen2702@users.noreply.github.com>
2022-12-10 21:37:45 +0700

    fix-code-N

--- My broad/myboard.js
+++
@@ -1,9 +0,0 @@
-// function myFunction() {
-//     var x = document.getElementsByClassName("board-content__tab");
-//     console.log(x);
-//     // if (x.style.display === "block") {
-//     //   x.style.display = "none";
-//     // } else {
-//     //   x.style.display = "block";
-//     // }
-//   }
